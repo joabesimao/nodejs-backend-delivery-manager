13934779f7671d8c78cde78aaaf9ac2e28784325
joabesimao <simaojoabecosta@gmail.com>
2025-01-05 13:34:40 -0300

    feat: ensure should call addRegister with correct values

--- src/presentation/controllers/addRegister/addRegister.ts
+++ src/presentation/controllers/addRegister/addRegister.ts
@@ -1,11 +1,16 @@
 import { AddRegister } from "../../../domain/usescases/addRegister/add-register";
+import { serverError } from "../../helpers/http/http-helper";
 import { Controller } from "../../protocols/controller";
 import { HttpRequest, HttpResponse } from "../../protocols/http";
 
 export class AddRegisterController implements Controller {
   constructor(private readonly addRegister: AddRegister) {}
-  handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    this.addRegister.add(httpRequest.body);
-    return null;
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      await this.addRegister.add(httpRequest.body);
+      return null;
+    } catch (error) {
+      return serverError(error);
+    }
   }
 }
