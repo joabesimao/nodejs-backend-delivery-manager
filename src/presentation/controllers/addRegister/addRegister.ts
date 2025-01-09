271606a387cfcb2bb4147c6e81a92b1473eb15d8
joabesimao <simaojoabecosta@gmail.com>
2025-01-09 14:13:41 -0300

    feat: ensure AddRegister returns 200 on sucess

--- src/presentation/controllers/addRegister/addRegister.ts
+++ src/presentation/controllers/addRegister/addRegister.ts
@@ -1,5 +1,5 @@
 import { AddRegister } from "../../../domain/usescases/addRegister/add-register";
-import { serverError } from "../../helpers/http/http-helper";
+import { noContent, ok, serverError } from "../../helpers/http/http-helper";
 import { Controller } from "../../protocols/controller";
 import { HttpRequest, HttpResponse } from "../../protocols/http";
 
@@ -7,8 +7,9 @@ export class AddRegisterController implements Controller {
   constructor(private readonly addRegister: AddRegister) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      await this.addRegister.add(httpRequest.body);
-      return null;
+      const { name, address, phone, quantity } = httpRequest.body;
+      await this.addRegister.add({ name, address, phone, quantity });
+      return noContent();
     } catch (error) {
       return serverError(error);
     }
