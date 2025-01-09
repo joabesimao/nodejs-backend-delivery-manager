54287c8c351870525ff93ce43d0b8a25db705009
joabesimao <simaojoabecosta@gmail.com>
2025-01-09 14:34:50 -0300

    feat: should return 4000 if validation fails

--- src/presentation/controllers/addRegister/addRegister.ts
+++ src/presentation/controllers/addRegister/addRegister.ts
@@ -1,12 +1,25 @@
 import { AddRegister } from "../../../domain/usescases/addRegister/add-register";
-import { noContent, ok, serverError } from "../../helpers/http/http-helper";
+import {
+  badRequest,
+  noContent,
+  ok,
+  serverError,
+} from "../../helpers/http/http-helper";
 import { Controller } from "../../protocols/controller";
 import { HttpRequest, HttpResponse } from "../../protocols/http";
+import { Validation } from "../../protocols/validation";
 
 export class AddRegisterController implements Controller {
-  constructor(private readonly addRegister: AddRegister) {}
+  constructor(
+    private readonly addRegister: AddRegister,
+    private readonly validation: Validation
+  ) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      const error = this.validation.validate(httpRequest.body);
+      if (error) {
+        return badRequest(error);
+      }
       const { name, address, phone, quantity } = httpRequest.body;
       await this.addRegister.add({ name, address, phone, quantity });
       return noContent();
