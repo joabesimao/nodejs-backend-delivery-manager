12d3fb408b76270312d30b182f26b94c319e22c7
joabesimao <simaojoabecosta@gmail.com>
2025-01-21 20:57:39 -0300

    refactor: move map to MongoHelper

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,4 +1,5 @@
 import { Collection, MongoClient } from "mongodb";
+import { RegisterModel } from "../../../../domain/models/register/register-model";
 
 export const MongoHelper = {
   client: null as MongoClient,
@@ -13,5 +14,10 @@ export const MongoHelper = {
 
   getCollection(name: string): Collection {
     return this.client.db().collection(name);
+  },
+
+  map: (collection: any): any => {
+    const { _id, ...collectionWithoutId } = collection;
+    return Object.assign({}, collectionWithoutId, { id: _id });
   },
 };
---
+++ src/infra/db/mongodb/register-repository/register-mapper.ts
@@ -0,0 +1,8 @@
+/* import { RegisterModel } from "../../../../domain/models/register/register-model";
+
+export const map = (register:any) :RegisterModel => {
+  const {_id,...registerWithoutId} = register
+  return Object.assign({},registerWithoutId,{id:_id})
+
+} 
+ */
