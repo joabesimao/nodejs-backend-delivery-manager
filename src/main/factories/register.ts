60ff706fb0ba3292a307541a8c76e8f752dcdf4a
joabesimao <simaojoabecosta@gmail.com>
2025-02-01 20:37:52 -0300

    feat: ensure LogControllerDecorator calls controller with correct value

--- src/main/factories/register.ts
+++ src/main/factories/register.ts
@@ -1,11 +1,14 @@
 import { AddRegisterController } from "../../presentation/controllers/addRegister/addRegister";
 import { DbAddRegister } from "../../data/usescases/addRegister/db-addRegister";
 import { RegisterMongoRepository } from "../../infra/db/mongodb/register-repository/register";
+import { LogControllerDecorator } from "../decorators/log";
+import { Controller } from "../../presentation/protocols/controller";
 
-export const makeRegisterController = (): AddRegisterController => {
+export const makeRegisterController = (): Controller => {
   const registerRepository = new RegisterMongoRepository();
 
   const addRegister = new DbAddRegister(registerRepository);
   const registerController = new AddRegisterController(addRegister);
-  return registerController;
+
+  return new LogControllerDecorator(registerController);
 };
