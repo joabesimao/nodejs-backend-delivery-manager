962d038aa1617e03d30cd0538991ec05990dfc32
joabesimao <simaojoabecosta@gmail.com>
2025-02-03 21:12:50 -0300

    feat: ensure LogControllerDecorator calls LogError repository if controller returns a server error

--- src/main/decorators/log.ts
+++ src/main/decorators/log.ts
@@ -1,11 +1,18 @@
+import { LogErrorRepository } from "../../data/protocols/db/register/log-error-repository";
 import { Controller } from "../../presentation/protocols/controller";
 import { HttpRequest, HttpResponse } from "../../presentation/protocols/http";
 
 export class LogControllerDecorator implements Controller {
-  constructor(private readonly controller: Controller) {}
+  constructor(
+    private readonly controller: Controller,
+    private readonly logErrorRepository: LogErrorRepository
+  ) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const result = await this.controller.handle(httpRequest);
+    if (result.statusCode === 500) {
+      await this.logErrorRepository.log(result.body.stack);
+    }
     return result;
   }
 }
