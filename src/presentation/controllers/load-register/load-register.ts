19f063609d0420e627113f618531a0b854a1897f
joabesimao <simaojoabecosta@gmail.com>
2025-02-06 10:48:29 -0300

    feat: ensure LoadRegisterController returns 500 if LoadRegisterUseCase throws

--- src/presentation/controllers/load-register/load-register.ts
+++ src/presentation/controllers/load-register/load-register.ts
@@ -1,5 +1,5 @@
 import { LoadRegisters } from "../../../domain/usescases/loadRegister/load-register";
-import { ok } from "../../helpers/http/http-helper";
+import { ok, serverError } from "../../helpers/http/http-helper";
 import { Controller } from "../../protocols/controller";
 import { HttpRequest, HttpResponse } from "../../protocols/http";
 
@@ -7,7 +7,12 @@ export class LoadRegistersController implements Controller {
   constructor(private readonly loadRegister: LoadRegisters) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const registers = await this.loadRegister.load();
-    return ok(registers);
+    try {
+      const registers = await this.loadRegister.load();
+      return ok(registers);
+    } catch (error) {
+      return serverError(error);
+    }
   }
 }
+
