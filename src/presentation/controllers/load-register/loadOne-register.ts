97aa6c0244cbe9223fc081d4683abb913fe57d61
joabesimao <simaojoabecosta@gmail.com>
2025-02-08 20:46:20 -0300

    feat: ensure return 204 if return loadOneRegister empty

--- src/presentation/controllers/load-register/loadOne-register.ts
+++ src/presentation/controllers/load-register/loadOne-register.ts
@@ -1,5 +1,5 @@
 import { LoadOneRegisters } from "../../../domain/usescases/loadRegister/loadOne-register";
-import { ok } from "../../helpers/http/http-helper";
+import { noContent, ok } from "../../helpers/http/http-helper";
 import { Controller } from "../../protocols/controller";
 import { HttpRequest, HttpResponse } from "../../protocols/http";
 
@@ -7,6 +7,6 @@ export class LoadOneRegistersController implements Controller {
   constructor(private readonly loadOneRegisters: LoadOneRegisters) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const result = await this.loadOneRegisters.loadById(httpRequest.body.id);
-    return ok(result);
+    return result ? ok(result) : noContent();
   }
 }
