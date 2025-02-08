bb092170cbe521eaf842fa622db4311233df9ad2
joabesimao <simaojoabecosta@gmail.com>
2025-02-08 19:32:00 -0300

    test: ensure load registers route return 200 if access on success

--- src/main/routes/register-route.test.ts
+++ src/main/routes/register-route.test.ts
@@ -1,8 +1,11 @@
 import request from "supertest";
 import app from "../config/app";
 import { MongoHelper } from "../../infra/db/mongodb/helpers/mongo-helper";
+import { Collection } from "mongodb";
 
-describe("Register Routes", () => {
+let regCollection: Collection;
+
+describe("Register Routes POST/registers", () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL as string);
   });
@@ -12,10 +15,10 @@ describe("Register Routes", () => {
   });
 
   beforeEach(async () => {
-    const regCollection = await MongoHelper.getCollection("registers");
+    regCollection = await MongoHelper.getCollection("registers");
     await regCollection.deleteMany({});
   });
-  
+
   test("Should return an register on success", async () => {
     await request(app)
       .post("/api/register")
@@ -36,5 +39,43 @@ describe("Register Routes", () => {
         amount: 1,
       })
       .expect(200);
+  });
+});
+
+describe("GET /Register", () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconect();
+  });
+
+  beforeEach(async () => {
+    regCollection = await MongoHelper.getCollection("registers");
+    await regCollection.deleteMany({});
+  });
+
+  test("Should return 200 on load registers", async () => {
+    await regCollection.insertOne(
+      {
+        id: 1,
+        client: {
+          id: 2,
+          name: "any_name",
+          lastName: "any_last_name",
+          phone: "any_number",
+        },
+        address: {
+          street: "any_street",
+          neighborhood: "any_neighborhood",
+          numberHouse: 1,
+          reference: "any_reference",
+        },
+        amount: 2,
+        quantity: "any_quantity",
+      },
+    );
+    await request(app).get("/api/register").expect(200);
   });
 });
