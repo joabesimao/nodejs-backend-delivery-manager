520aa9bd7fa1e47f29162e351d6212fea1626f48
joabesimao <simaojoabecosta@gmail.com>
2025-02-08 19:06:54 -0300

    feat: ensure load registers route return 200 if access on success

--- src/main/routes/register-routes.ts
+++ src/main/routes/register-routes.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { makeRegisterController } from "../factories/register";
 import { adaptRoute } from "../../main/adapters/express-route-adapter";
+import { makeLoadRegisterController } from "../factories/load-register";
 
 export default (router: Router): void => {
   router.post("/register", adaptRoute(makeRegisterController()));
+  router.get("/register", adaptRoute(makeLoadRegisterController()));
 };
