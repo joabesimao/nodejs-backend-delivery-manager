beeeebf7837d7586058f16de5b1f1cd664eac36d
joabesimao <simaojoabecosta@gmail.com>
2025-02-08 21:03:49 -0300

    feat: ensure return 500 if loadOneRegister throws

--- src/presentation/controllers/load-register/loadOne-register.ts
+++ src/presentation/controllers/load-register/loadOne-register.ts
@@ -1,12 +1,16 @@
 import { LoadOneRegisters } from "../../../domain/usescases/loadRegister/loadOne-register";
-import { noContent, ok } from "../../helpers/http/http-helper";
+import { noContent, ok, serverError } from "../../helpers/http/http-helper";
 import { Controller } from "../../protocols/controller";
 import { HttpRequest, HttpResponse } from "../../protocols/http";
 
 export class LoadOneRegistersController implements Controller {
   constructor(private readonly loadOneRegisters: LoadOneRegisters) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const result = await this.loadOneRegisters.loadById(httpRequest.body.id);
-    return result ? ok(result) : noContent();
+    try {
+      const result = await this.loadOneRegisters.loadById(httpRequest.body.id);
+      return result ? ok(result) : noContent();
+    } catch (error) {
+      return serverError(error);
+    }
   }
 }
