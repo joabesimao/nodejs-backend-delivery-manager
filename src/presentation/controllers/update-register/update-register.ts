1e316a7c173631bb3991ce3da49ccbdb72715eee
joabesimao <simaojoabecosta@gmail.com>
2025-02-16 12:29:20 -0300

    chore: add try and catch

--- src/presentation/controllers/update-register/update-register.ts
+++ src/presentation/controllers/update-register/update-register.ts
@@ -1,16 +1,20 @@
 import { UpdateRegister } from "../../../domain/usescases/updateRegister/update-register";
-import { ok } from "../../helpers/http/http-helper";
+import { ok, serverError } from "../../helpers/http/http-helper";
 import { Controller } from "../../protocols/controller";
 import { HttpRequest, HttpResponse } from "../../protocols/http";
 
 export class UpdateRegisterController implements Controller {
   constructor(private readonly update: UpdateRegister) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const updateData = await this.update.update(
-      httpRequest.params.id,
-      httpRequest.body
-    );
+    try {
+      const updateData = await this.update.update(
+        httpRequest.params.id,
+        httpRequest.body
+      );
 
-    return ok("updateData");
+      return ok(updateData);
+    } catch (error) {
+      return serverError(error);
+    }
   }
 }
