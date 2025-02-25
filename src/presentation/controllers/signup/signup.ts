b7454f1fde275a5e1eb69ce74d813f4dc954638c
joabesimao <simaojoabecosta@gmail.com>
2025-02-25 19:57:59 -0300

    feat: ensure SignUpController return 400 if an invalid email provided

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,11 +1,15 @@
 import { AddAccount } from "../../../domain/usescases/signup/add-account";
-import { MissingParamError } from "../../errors";
+import { InvalidParamError, MissingParamError } from "../../errors";
 import { badRequest, ok, serverError } from "../../helpers/http/http-helper";
 import { Controller } from "../../protocols/controller";
 import { HttpRequest, HttpResponse } from "../../protocols/http";
+import { EmailValidator } from "../../protocols/email-validator";
 
 export class SignupController implements Controller {
-  constructor(private readonly addAccount: AddAccount) {}
+  constructor(
+    private readonly addAccount: AddAccount,
+    private readonly emailValidator: EmailValidator
+  ) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { name, email, password, passwordConfirmation } = httpRequest.body;
@@ -19,6 +23,10 @@ export class SignupController implements Controller {
         if (!httpRequest.body[field]) {
           return badRequest(new MissingParamError(field));
         }
+      }
+      const isValid = await this.emailValidator.isValid(email);
+      if (!isValid) {
+        return badRequest(new InvalidParamError("email"));
       }
       const account = await this.addAccount.add({
         name: name,
