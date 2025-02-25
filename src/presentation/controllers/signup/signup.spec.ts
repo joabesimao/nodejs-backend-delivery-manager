9accc79f4abd134eaace86e621267eb74f234f39
joabesimao <simaojoabecosta@gmail.com>
2025-02-24 22:24:22 -0300

    test: ensure return 200 on success

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -1,16 +1,40 @@
 import { SignupController } from "./signup";
+import {
+  AddAccount,
+  AddAccountModel,
+} from "../../../domain/usescases/signup/add-account";
 import { HttpRequest } from "../../protocols/http";
 import { MissingParamError } from "../../errors";
-import { badRequest } from "../../helpers/http/http-helper";
+import { badRequest, ok, serverError } from "../../helpers/http/http-helper";
+import { AccountModel } from "../../../domain/models/account/account-model";
 
+const makeFakeAccountModel = (): AccountModel => ({
+  id: 1,
+  name: "any_name",
+  email: "any_email@email.com",
+  password: "any_password",
+  passwordConfirmation: "any_password",
+});
+
+const makeAddAccount = (): AddAccount => {
+  class AddAccountStub implements AddAccount {
+    async add(account: AddAccountModel): Promise<AccountModel> {
+      return new Promise((resolve) => resolve(makeFakeAccountModel()));
+    }
+  }
+  return new AddAccountStub();
+};
 interface SutTypes {
   sut: SignupController;
+  addAccountStub: AddAccount;
 }
 
 const makeSut = (): SutTypes => {
-  const sut = new SignupController();
+  const addAccountStub = makeAddAccount();
+  const sut = new SignupController(addAccountStub);
   return {
     sut,
+    addAccountStub,
   };
 };
 
@@ -84,5 +108,39 @@ describe("Signup Controller", () => {
     expect(httpResponse).toEqual(
       badRequest(new MissingParamError("passwordConfirmation"))
     );
+  });
+
+  test("Should call addAccount with correct values", async () => {
+    const { sut, addAccountStub } = makeSut();
+    const spy = jest.spyOn(addAccountStub, "add");
+
+    await sut.handle(makeRequest());
+
+    expect(spy).toHaveBeenCalledWith({
+      name: "any_name",
+      email: "any_email@email.com",
+      password: "any_password",
+      passwordConfirmation: "any_password",
+    });
+  });
+
+  test("Should return 200 on success", async () => {
+    const { sut } = makeSut();
+
+    const httpResponse = await sut.handle(makeRequest());
+
+    expect(httpResponse).toEqual(ok(makeFakeAccountModel()));
+  });
+
+  test("Should return 500 if addAccount throws", async () => {
+    const { sut, addAccountStub } = makeSut();
+    jest
+      .spyOn(addAccountStub, "add")
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error()))
+      );
+
+    const httpResponse = await sut.handle(makeRequest());
+    expect(httpResponse).toEqual(serverError(new Error()));
   });
 });
