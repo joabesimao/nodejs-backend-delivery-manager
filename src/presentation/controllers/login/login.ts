8669403f88769ba2a34118483fb495e369a84850
joabesimao <simaojoabecosta@gmail.com>
2025-03-05 21:58:57 -0300

    feat: ensure LoginController return 400 if an invalid email is provided

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -2,7 +2,7 @@ import { Controller } from "../../protocols/controller";
 import { HttpRequest, HttpResponse } from "../../protocols/http";
 import { EmailValidator } from "../../protocols/email-validator";
 import { badRequest } from "../../helpers/http/http-helper";
-import { MissingParamError } from "../../errors";
+import { InvalidParamError, MissingParamError } from "../../errors";
 
 export class LoginController implements Controller {
   constructor(private readonly emailValidator: EmailValidator) {}
@@ -16,6 +16,9 @@ export class LoginController implements Controller {
         );
       }
     }
-    await this.emailValidator.isValid(email);
+    const isValid = await this.emailValidator.isValid(email);
+    if (!isValid) {
+      return badRequest(new InvalidParamError("email"));
+    }
   }
 }
