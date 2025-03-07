656fcbcfdd6c5b17927297db7d2abfd71effbc18
joabesimao <simaojoabecosta@gmail.com>
2025-03-07 20:41:41 -0300

    refactor: ensure add LogControllerDecorator to SignUpController

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,10 +1,11 @@
 import { DbAddAccount } from "../../data/usescases/add-account/db-add-account";
 import { BcryptAdapter } from "../../infra/cryptography/bcrypt-adapter";
+import { LogMongoRepository } from "../../infra/db/mongodb/log-repository/log-mongo-repository";
 import { AddAccountMongoRepository } from "../../infra/db/mongodb/signup-repository/signup-repository";
 import { SignupController } from "../../presentation/controllers/signup/signup";
-import { ValidationComposite } from "../../presentation/helpers/validator/validation-composite";
 import { Controller } from "../../presentation/protocols/controller";
 import { EmailValidatorAdapter } from "../../utils/email-validator-adapter";
+import { LogControllerDecorator } from "../decorators/log";
 import { makeSignupValidation } from "./signup-validation";
 
 export const makeSignupController = (): Controller => {
@@ -13,10 +14,11 @@ export const makeSignupController = (): Controller => {
   const encrypt = new BcryptAdapter(salt);
   const accountRepository = new AddAccountMongoRepository();
   const addAccount = new DbAddAccount(encrypt, accountRepository);
-
-  return new SignupController(
+  const signupController = new SignupController(
     addAccount,
     emailValidator,
     makeSignupValidation()
   );
+  const logError = new LogMongoRepository();
+  return new LogControllerDecorator(signupController, logError);
 };
