2dc040778a4ac5c0dfdfa962dd250d461705bc09
joabesimao <simaojoabecosta@gmail.com>
2025-03-07 19:12:56 -0300

    feat: ensure return Validator with correct value

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -4,14 +4,17 @@ import { badRequest, ok, serverError } from "../../helpers/http/http-helper";
 import { Controller } from "../../protocols/controller";
 import { HttpRequest, HttpResponse } from "../../protocols/http";
 import { EmailValidator } from "../../protocols/email-validator";
+import { Validation } from "../../helpers/validator/validation";
 
 export class SignupController implements Controller {
   constructor(
     private readonly addAccount: AddAccount,
-    private readonly emailValidator: EmailValidator
+    private readonly emailValidator: EmailValidator,
+    private readonly validation: Validation
   ) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      await this.validation.validate(httpRequest.body);
       const { name, email, password, passwordConfirmation } = httpRequest.body;
       const requireFields = [
         "name",
