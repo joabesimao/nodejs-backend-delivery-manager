9b2025425dc50af74ae8b7da7a45c0c188fb4f9b
joabesimao <simaojoabecosta@gmail.com>
2025-03-07 19:43:32 -0300

    refactor: remove unused code

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -77,68 +77,6 @@ const makeRequest = (): HttpRequest => ({
 });
 
 describe("Signup Controller", () => {
-  test("Should return 400 if no name is provided", async () => {
-    const { sut } = makeSut();
-    const httpMissingParamName = {
-      body: {
-        email: "any_email@email.com",
-        password: "any_password",
-        passwordConfirmation: "any_password",
-      },
-    };
-
-    const httpResponse = await sut.handle(httpMissingParamName);
-
-    expect(httpResponse).toEqual(badRequest(new MissingParamError("name")));
-  });
-
-  test("Should return 400 if no email is provided", async () => {
-    const { sut } = makeSut();
-    const httpMissingParamEmail = {
-      body: {
-        name: "any_name",
-        password: "any_password",
-        passwordConfirmation: "any_password",
-      },
-    };
-
-    const httpResponse = await sut.handle(httpMissingParamEmail);
-
-    expect(httpResponse).toEqual(badRequest(new MissingParamError("email")));
-  });
-
-  test("Should return 400 if no password is provided", async () => {
-    const { sut } = makeSut();
-    const httpMissingParamEmail = {
-      body: {
-        name: "any_name",
-        email: "any_email@email.com",
-        passwordConfirmation: "any_password",
-      },
-    };
-
-    const httpResponse = await sut.handle(httpMissingParamEmail);
-
-    expect(httpResponse).toEqual(badRequest(new MissingParamError("password")));
-  });
-
-  test("Should return 400 if no passwordConfirmation is provided", async () => {
-    const { sut } = makeSut();
-    const httpMissingParamEmail = {
-      body: {
-        name: "any_name",
-        email: "any_email@email.com",
-        password: "any_password",
-      },
-    };
-
-    const httpResponse = await sut.handle(httpMissingParamEmail);
-
-    expect(httpResponse).toEqual(
-      badRequest(new MissingParamError("passwordConfirmation"))
-    );
-  });
-
   test("Should call addAccount with correct values", async () => {
     const { sut, addAccountStub } = makeSut();
     const spy = jest.spyOn(addAccountStub, "add");
