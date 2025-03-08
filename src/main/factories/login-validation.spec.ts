3de0837c1c781c7ec26a94dce3c4f058ce6ed1fd
joabesimao <simaojoabecosta@gmail.com>
2025-03-08 20:57:56 -0300

    test: add LoginValidationFactory

---
+++ src/main/factories/login-validation.spec.ts
@@ -0,0 +1,30 @@
+import { ValidationComposite } from "../../presentation/helpers/validator/validation-composite";
+import { makeLoginValidation } from "./login-validation";
+import { RequireFieldsValidation } from "../../presentation/helpers/validator/require-field-validation";
+import { CompareFieldsValidation } from "../../presentation/helpers/validator/compare-fields-validation";
+import { Validation } from "../../presentation/helpers/validator/validation";
+import { EmailValidation } from "../../presentation/helpers/validator/email-validation";
+import { EmailValidator } from "../../presentation/protocols/email-validator";
+
+jest.mock("../../presentation/helpers/validator/validation-composite");
+
+const makeEmailValidator = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    async isValid(email: string): Promise<boolean> {
+      return new Promise((resolve) => resolve(true));
+    }
+  }
+  return new EmailValidatorStub();
+};
+
+describe("Login validation Factory", () => {
+  test("Should call ValidationComposite with all validations", async () => {
+    makeLoginValidation();
+    const validations: Validation[] = [];
+    for (const field of ["email", "password"]) {
+      validations.push(new RequireFieldsValidation(field));
+    }
+    validations.push(new EmailValidation("email", makeEmailValidator()));
+    expect(ValidationComposite).toHaveBeenCalledWith(validations);
+  });
+});
