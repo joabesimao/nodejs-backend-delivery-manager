1ac3fc535c04cda298f8a2b0075c8ffd2891b287
joabesimao <simaojoabecosta@gmail.com>
2025-03-15 18:54:18 -0300

    feat: ensure authMiddleware return 403 if no headers are found

---
+++ src/presentation/middlewares/auth-middleware.ts
@@ -0,0 +1,10 @@
+import { AccessDeniedError } from "../errors/access-denied-error";
+import { forbidden } from "../helpers/http/http-helper";
+import { HttpRequest, HttpResponse } from "../protocols/http";
+import { Middleware } from "../protocols/middleware";
+
+export class AuthMiddleware implements Middleware {
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    return forbidden(new AccessDeniedError());
+  }
+}
