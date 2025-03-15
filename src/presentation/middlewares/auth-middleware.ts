2c48537263f21c7a9dedc1eacb2853116438b986
joabesimao <simaojoabecosta@gmail.com>
2025-03-15 19:55:43 -0300

    feat: ensure authMiddleware return 200 if LoadAccountByToken  success

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,15 +1,19 @@
 import { AccessDeniedError } from "../errors/access-denied-error";
-import { forbidden } from "../helpers/http/http-helper";
+import { forbidden, ok } from "../helpers/http/http-helper";
 import { HttpRequest, HttpResponse } from "../protocols/http";
 import { Middleware } from "../protocols/middleware";
 import { LoadAccountByToken } from "../../domain/usescases/auth-middleware/load-account-by-token";
+
 
 export class AuthMiddleware implements Middleware {
   constructor(private readonly loadAccountByAccessToken: LoadAccountByToken) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const accessToken = httpRequest.headers?.["x-access-token"];
     if (accessToken) {
-      await this.loadAccountByAccessToken.load(accessToken);
+      const account = await this.loadAccountByAccessToken.load(accessToken);
+      if (account) {
+        return ok({ accountId: account.id }) as unknown as any;
+      }
     }
     return forbidden(new AccessDeniedError());
   }
