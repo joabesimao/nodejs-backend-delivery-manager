5e035ed9bfd26da1407fa465a341d23212352a16
joabesimao <simaojoabecosta@gmail.com>
2025-03-16 11:34:39 -0300

    feat: ensure AccountMongoRepository find a account by token without role

--- src/infra/db/mongodb/account-repository/account-repository.ts
+++ src/infra/db/mongodb/account-repository/account-repository.ts
@@ -1,13 +1,28 @@
 import { ObjectId } from "mongodb";
 import { UpdateAccessTokenRepository } from "../../../../data/protocols/db/access-token-repository/update-access-token-repository";
 import { FindAccountByEmailRepository } from "../../../../data/protocols/db/account/find-account-by-email-repository";
+import { LoadAccountByTokenRepository } from "../../../../data/protocols/db/account/load-account-by-token-repository";
 import { AccountModel } from "../../../../domain/models/account/account-model";
 import { MongoHelper } from "../helpers/mongo-helper";
 import { map } from "../register-repository/register-mapper";
 
 export class AccountMongoRepository
-  implements FindAccountByEmailRepository, UpdateAccessTokenRepository
+  implements
+    FindAccountByEmailRepository,
+    UpdateAccessTokenRepository,
+    LoadAccountByTokenRepository
 {
+  async loadByToken(token: string, role?: string): Promise<AccountModel> {
+    const accountCollection = await MongoHelper.getCollection("accounts");
+    const account = await accountCollection.findOne({
+      accessToken: token,
+    });
+    if (!account) {
+      return null;
+    }
+    return map(account);
+  }
+
   async updateAccessToken(id: number, token: string): Promise<void> {
     const objectId = new ObjectId(id);
     const accountCollection = await MongoHelper.getCollection("accounts");
