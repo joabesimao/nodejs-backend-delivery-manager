28f6a9815209662a008c5e91a421fd02c4388646
joabesimao <simaojoabecosta@gmail.com>
2025-03-20 20:47:48 -0300

    feat: ensure LoadOrderDelivery returns  500 if loadDeliverys throws

--- src/presentation/controllers/load-order-delivery/load-order-delivery.ts
+++ src/presentation/controllers/load-order-delivery/load-order-delivery.ts
@@ -1,6 +1,6 @@
 import { LoadOrderDelivery } from "../../../domain/usescases/order-delivery/load-order-delivery";
 import { Validation } from "../../protocols/validation";
-import { badRequest, ok } from "../../helpers/http/http-helper";
+import { badRequest, ok, serverError } from "../../helpers/http/http-helper";
 import { Controller } from "../../protocols/controller";
 import { HttpRequest, HttpResponse } from "../../protocols/http";
 
@@ -10,11 +10,15 @@ export class LoadOrderDeliveryController implements Controller {
     private readonly validation: Validation
   ) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = await this.validation.validate(httpRequest.body);
-    if(error){
-      return badRequest(error)
+    try {
+      const error = await this.validation.validate(httpRequest.body);
+      if (error) {
+        return badRequest(error);
+      }
+      const allOrders = await this.loadOrderDelivery.loadAll();
+      return ok(allOrders);
+    } catch (error) {
+      return serverError(error);
     }
-    const allOrders = await this.loadOrderDelivery.loadAll();
-    return ok(allOrders);
   }
 }
