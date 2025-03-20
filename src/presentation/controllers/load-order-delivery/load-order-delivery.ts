bfed2ef51ef671092a8a4d7235407b56b77fd94e
joabesimao <simaojoabecosta@gmail.com>
2025-03-20 20:26:19 -0300

    feat: ensure LoadOrderDelivery call validation with correct values

--- src/presentation/controllers/load-order-delivery/load-order-delivery.ts
+++ src/presentation/controllers/load-order-delivery/load-order-delivery.ts
@@ -1,11 +1,19 @@
 import { LoadOrderDelivery } from "../../../domain/usescases/order-delivery/load-order-delivery";
-import { ok } from "../../helpers/http/http-helper";
+import { Validation } from "../../protocols/validation";
+import { badRequest, ok } from "../../helpers/http/http-helper";
 import { Controller } from "../../protocols/controller";
 import { HttpRequest, HttpResponse } from "../../protocols/http";
 
 export class LoadOrderDeliveryController implements Controller {
-  constructor(private readonly loadOrderDelivery: LoadOrderDelivery) {}
+  constructor(
+    private readonly loadOrderDelivery: LoadOrderDelivery,
+    private readonly validation: Validation
+  ) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    const error = await this.validation.validate(httpRequest.body);
+    if(error){
+      return badRequest(error)
+    }
     const allOrders = await this.loadOrderDelivery.loadAll();
     return ok(allOrders);
   }
