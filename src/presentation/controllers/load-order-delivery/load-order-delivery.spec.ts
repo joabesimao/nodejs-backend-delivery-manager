eb3498f3a66a3d6ff5f27b40fde0f9f260c05487
joabesimao <simaojoabecosta@gmail.com>
2025-03-20 20:05:10 -0300

    test: ensure LoadOrderDelivery call the list of orders

---
+++ src/presentation/controllers/load-order-delivery/load-order-delivery.spec.ts
@@ -0,0 +1,115 @@
+import { LoadOrderDelivery } from "../../../domain/usescases/order-delivery/load-order-delivery";
+import { LoadOrderDeliveryController } from "./load-order-delivery";
+import { HttpRequest } from "../../protocols/http";
+import { ok, serverError } from "../../helpers/http/http-helper";
+import { Validation } from "../../protocols/validation";
+import { RegisterModel } from "../../../domain/models/register/register-model";
+import { MissingParamError } from "../../errors";
+import { OrderDeliveryModel } from "../../../domain/models/order-delivery/order-delivery";
+
+const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    id: "1",
+    register: {
+      id: 1,
+      client: {
+        name: "any_name",
+        lastName: "any_last_name",
+        phone: "any_phone",
+      },
+      address: {
+        street: "any_street",
+        neighborhood: "any_neighborhood",
+        numberHouse: 123,
+        reference: "any_reference",
+        city: "any_city",
+      },
+    },
+    amount: 1,
+    data: new Date("2022-10-10"),
+    quantity: "12",
+  },
+});
+const makeOrdersDelivery = (): OrderDeliveryModel[] => [
+  {
+    id: "1",
+    register: {
+      id: 1,
+      client: {
+        name: "any_name",
+        lastName: "any_last_name",
+        phone: "any_phone",
+      },
+      address: {
+        street: "any_street",
+        neighborhood: "any_neighborhood",
+        numberHouse: 123,
+        reference: "any_reference",
+        city: "any_city",
+      },
+    },
+    amount: 1,
+    data: new Date("2022-10-10"),
+    quantity: "12",
+  },
+  {
+    id: "1",
+    register: {
+      id: 1,
+      client: {
+        name: "any_name",
+        lastName: "any_last_name",
+        phone: "any_phone",
+      },
+      address: {
+        street: "any_street",
+        neighborhood: "any_neighborhood",
+        numberHouse: 123,
+        reference: "any_reference",
+        city: "any_city",
+      },
+    },
+    amount: 1,
+    data: new Date("2022-10-10"),
+    quantity: "12",
+  },
+];
+interface SutTypes {
+  sut: LoadOrderDeliveryController;
+  loadOrderDeliveryStub: LoadOrderDelivery;
+}
+const makeLoadOrderDeliveryStub = (): LoadOrderDelivery => {
+  class LoadOrderStub implements LoadOrderDelivery {
+    async loadAll(): Promise<OrderDeliveryModel[]> {
+      return new Promise((resolve) => resolve(makeOrdersDelivery()));
+    }
+  }
+  return new LoadOrderStub();
+};
+
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    validate(input: any): Error {
+      return null as any;
+    }
+  }
+  return new ValidationStub();
+};
+
+const makeSut = (): SutTypes => {
+  const loadOrderDeliveryStub = makeLoadOrderDeliveryStub();
+  const sut = new LoadOrderDeliveryController(loadOrderDeliveryStub);
+  return {
+    sut,
+    loadOrderDeliveryStub,
+  };
+};
+describe("LoadOrderDelivery Controller", () => {
+  test("Should call LoadOrderDelivery with correct values", async () => {
+    const { sut, loadOrderDeliveryStub } = makeSut();
+    const loadRegisterSpy = jest.spyOn(loadOrderDeliveryStub, "loadAll");
+    const fakeRequest = makeFakeRequest();
+    await sut.handle(fakeRequest);
+    expect(loadRegisterSpy).toHaveBeenCalled();
+  });
+});
