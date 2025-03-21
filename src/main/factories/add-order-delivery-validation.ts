8b7b314428e080288cfb9a656c66b07f01686a75
joabesimao <simaojoabecosta@gmail.com>
2025-03-21 14:36:17 -0300

    feat: ensure call ValidationComposite with all validations of Add order delivery

---
+++ src/main/factories/add-order-delivery-validation.ts
@@ -0,0 +1,14 @@
+import {
+  ValidationComposite,
+  RequireFieldsValidation,
+} from "../../presentation/helpers/validators";
+import { Validation } from "../../presentation/protocols/validation";
+
+export const makeAddOrderDeliveryValidation = (): ValidationComposite => {
+  const validations: Validation[] = [];
+  for (const field of ["register", "amount", "data", "quantity"]) {
+    validations.push(new RequireFieldsValidation(field));
+  }
+
+  return new ValidationComposite(validations);
+};
