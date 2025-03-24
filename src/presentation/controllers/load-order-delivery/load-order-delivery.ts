9ec374398a3013bf70fb5984cc57909e4af38ae8
joabesimao <simaojoabecosta@gmail.com>
2025-03-24 14:49:25 -0300

    feat: ensure create factory order-delivery

--- src/presentation/controllers/load-order-delivery/load-order-delivery.ts
+++ src/presentation/controllers/load-order-delivery/load-order-delivery.ts
@@ -5,16 +5,9 @@ import { Controller } from "../../protocols/controller";
 import { HttpRequest, HttpResponse } from "../../protocols/http";
 
 export class LoadOrderDeliveryController implements Controller {
-  constructor(
-    private readonly loadOrderDelivery: LoadOrderDelivery,
-    private readonly validation: Validation
-  ) {}
+  constructor(private readonly loadOrderDelivery: LoadOrderDelivery) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const error = await this.validation.validate(httpRequest.body);
-      if (error) {
-        return badRequest(error);
-      }
       const allOrders = await this.loadOrderDelivery.loadAll();
       return ok(allOrders);
     } catch (error) {
