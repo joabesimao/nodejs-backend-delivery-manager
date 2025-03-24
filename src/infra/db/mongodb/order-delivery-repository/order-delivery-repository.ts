51d2b32c52f14531855e77e4a23477ab0e1d891b
joabesimao <simaojoabecosta@gmail.com>
2025-03-24 14:13:37 -0300

    feat: ensure OrderDeliveryRepository returns an list of  OrderDelivery on success

--- src/infra/db/mongodb/order-delivery-repository/order-delivery-repository.ts
+++ src/infra/db/mongodb/order-delivery-repository/order-delivery-repository.ts
@@ -1,11 +1,23 @@
 import { AddOrderDeliveryRepository } from "../../../../data/protocols/db/order-delivery/add-order-delivery";
+import { LoadOrderDeliveryRepository } from "../../../../data/protocols/db/order-delivery/load-order-delivery";
 import { OrderDeliveryModel } from "../../../../domain/models/order-delivery/order-delivery";
 import { AddOrderDeliveryModel } from "../../../../domain/usescases/order-delivery/add-order-delivery";
 import { MongoHelper } from "../helpers/mongo-helper";
 
 export class OrderDeliveryMongoRepository
-  implements AddOrderDeliveryRepository
+  implements AddOrderDeliveryRepository, LoadOrderDeliveryRepository
 {
+  async getAllOrderOfDelivery(): Promise<OrderDeliveryModel[]> {
+    const listOfOrdersOfDeliveryCollection = await MongoHelper.getCollection(
+      "orderDeliverys"
+    );
+    const allListOfOrdersOfDelivery = await listOfOrdersOfDeliveryCollection
+      .find()
+      .toArray();
+
+    return allListOfOrdersOfDelivery as unknown as OrderDeliveryModel[];
+  }
+
   async addOrderOfDelivery(
     orderOfDelivery: AddOrderDeliveryModel
   ): Promise<OrderDeliveryModel> {
