7abf860ba314d5e4677c728c8ae49ac8414c6896
joabesimao <simaojoabecosta@gmail.com>
2025-03-24 20:12:31 -0300

    feat: ensure return 500 if DeleteOrderDelivery throws

--- src/presentation/controllers/delete-order-delivery/delete-order-delivery.ts
+++ src/presentation/controllers/delete-order-delivery/delete-order-delivery.ts
@@ -1,12 +1,18 @@
 import { DeleteOrderDelivery } from "../../../domain/usescases/order-delivery/delete-order-delivery";
-import { ok } from "../../helpers/http/http-helper";
+import { ok, serverError } from "../../helpers/http/http-helper";
 import { Controller } from "../../protocols/controller";
 import { HttpRequest, HttpResponse } from "../../protocols/http";
 
 export class DeleteOrderDeliveryController implements Controller {
   constructor(private readonly deleteOrderDelivery: DeleteOrderDelivery) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const result = await this.deleteOrderDelivery.delete(httpRequest.params.id);
-    return ok(result);
+    try {
+      const result = await this.deleteOrderDelivery.delete(
+        httpRequest.params.id
+      );
+      return ok(result);
+    } catch (error) {
+      return serverError(error);
+    }
   }
 }
