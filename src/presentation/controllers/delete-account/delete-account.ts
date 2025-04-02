70ed1bcd10901ea723125d91fab18f26c2c4efc4
joabesimao <simaojoabecosta@gmail.com>
2025-04-02 20:55:00 -0300

    feat: ensure return a message when DeleteRegister on success

--- src/presentation/controllers/delete-account/delete-account.ts
+++ src/presentation/controllers/delete-account/delete-account.ts
@@ -1,13 +1,18 @@
 import { Controller } from "../../protocols/controller";
 import { HttpRequest, HttpResponse } from "../../protocols/http";
 import { DeleteAccount } from "../../../domain/usescases/delete-account/delete-account";
+import { ok, serverError } from "../../helpers/http/http-helper";
 
 export class DeleteAccountController implements Controller {
   constructor(private readonly deleteAccount: DeleteAccount) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const account = await this.deleteAccount.deleteAccountById(
-      httpRequest.params.id
-    );
-    return null;
+    try {
+      const account = await this.deleteAccount.deleteAccountById(
+        httpRequest.params.id
+      );
+      return ok(account);
+    } catch (error) {
+      return serverError(error);
+    }
   }
 }
