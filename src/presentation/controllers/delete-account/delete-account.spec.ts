91653174ef6a62ec3c2a113708b72212c91d486d
joabesimao <simaojoabecosta@gmail.com>
2025-04-02 20:57:33 -0300

    test: ensure return 500 if DeleteAccount throws

--- src/presentation/controllers/delete-account/delete-account.spec.ts
+++ src/presentation/controllers/delete-account/delete-account.spec.ts
@@ -1,7 +1,7 @@
 import { DeleteAccountController } from "./delete-account";
 import { DeleteAccount } from "../../../domain/usescases/delete-account/delete-account";
 import { HttpRequest } from "../../protocols/http";
-import { ok } from "../../helpers/http/http-helper";
+import { ok, serverError } from "../../helpers/http/http-helper";
 
 const makeDeleteAccountStub = (): DeleteAccount => {
   class DeleteAccountStub implements DeleteAccount {
@@ -59,5 +59,17 @@ describe("DeleteAccount Controller", () => {
 
     const deletedAccount = await sut.handle(fakehttpRequest());
     expect(deletedAccount).toEqual(ok("conta apagada com sucesso"));
+  });
+
+  test("Should return 500 if DeleteAccount throws", async () => {
+    const { sut, deleteAccountStub } = makeSut();
+    jest
+      .spyOn(deleteAccountStub, "deleteAccountById")
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error()))
+      );
+
+    const deletedRegister = await sut.handle(fakehttpRequest());
+    expect(deletedRegister).toEqual(serverError(new Error("")));
   });
 });
