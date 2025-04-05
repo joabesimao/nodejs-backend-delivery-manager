eed0e0e66f2cc8cd4256e8adcfd3f9dd8e1e8a68
joabesimao <simaojoabecosta@gmail.com>
2025-04-05 19:43:36 -0300

    feat: ensure DbAddRegister call RegisterByNameRepository with correct values

--- src/data/usescases/add-register/db-add-register.ts
+++ src/data/usescases/add-register/db-add-register.ts
@@ -4,9 +4,15 @@ import {
   AddRegisterModel,
 } from "./db-add-register-protocols";
 import { AddRegisterRepository } from "../../protocols/db/register/add-register-repository";
+import { LoadRegisterByNameRepository } from "../../protocols/db/register/load-register-repository";
 export class DbAddRegister implements AddRegister {
-  constructor(private readonly registerRepository: AddRegisterRepository) {}
+  constructor(
+    private readonly registerRepository: AddRegisterRepository,
+    private readonly registerByNameRepository: LoadRegisterByNameRepository
+  ) {}
+
   async add(data: AddRegisterModel): Promise<RegisterModel> {
+    await this.registerByNameRepository.findByName(data.client.name);
     const result = await this.registerRepository.add(data);
     return result;
   }
