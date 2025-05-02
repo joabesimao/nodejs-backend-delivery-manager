76c2c6be5eb5bde171970d32a489c43e14bb57ae
joabesimao <simaojoabecosta@gmail.com>
2025-05-02 12:56:51 -0300

    chore: ensure analise code of prisma

--- prisma/migrations/20250428232121_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `orderDeliveryId` on the `register` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `register` DROP COLUMN `orderDeliveryId`;
