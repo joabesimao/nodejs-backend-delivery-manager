923008ea9ca216c681d6015ca314be122ec96104
joabesimao <simaojoabecosta@gmail.com>
2025-05-06 13:27:58 -0300

    feat: ensure LoadAddressRepository return a list of address

--- src/infra/db/mysql/address-repository/address-repository.ts
+++ src/infra/db/mysql/address-repository/address-repository.ts
@@ -1,9 +1,12 @@
 import { AddAddressRepository } from "../../../../data/protocols/db/address/add-address";
+import { LoadAddressRepository } from "../../../../data/protocols/db/address/load-address";
 import { Address } from "../../../../domain/models/register/address-model";
 import { AddAddressModel } from "../../../../domain/usescases/add-address/add-address";
 import { prisma } from "../helpers";
 
-export class AddressMysqlRepository implements AddAddressRepository {
+export class AddressMysqlRepository
+  implements AddAddressRepository, LoadAddressRepository
+{
   async add(address: AddAddressModel): Promise<Address> {
     const createAddress = await prisma.address.create({
       data: {
@@ -15,5 +18,10 @@ export class AddressMysqlRepository implements AddAddressRepository {
       },
     });
     return createAddress;
+  }
+
+  async loadAll(): Promise<Address[]> {
+    const loadAllAddress = await prisma.address.findMany();
+    return loadAllAddress;
   }
 }
