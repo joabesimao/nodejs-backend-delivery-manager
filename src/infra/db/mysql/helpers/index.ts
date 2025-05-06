84e9b8f9a6665a9fbf47295cd9f05b0918085237
joabesimao <simaojoabecosta@gmail.com>
2025-05-06 14:36:10 -0300

    chore: ensure add config of the pool conection mysql in env

--- src/infra/db/mysql/helpers/index.ts
+++ src/infra/db/mysql/helpers/index.ts
@@ -1,12 +1,13 @@
+import env from "../../../../env";
 import mysql from "mysql2/promise";
 import { PrismaClient } from "@prisma/client";
 
 export let prisma = new PrismaClient();
 
 export const pool = mysql.createPool({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "rootpassword",
-  database: "CadClient",
+  host: env.host,
+  port: env.port,
+  user: env.user,
+  password: env.password,
+  database: env.database,
 });
