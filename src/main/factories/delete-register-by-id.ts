7ccbfb756043f55a41413619512acea896d23632
joabesimao <simaojoabecosta@gmail.com>
2025-05-10 20:53:54 -0300

    feat: ensure delete register

--- src/main/factories/delete-register-by-id.ts
+++ src/main/factories/delete-register-by-id.ts
@@ -1,14 +1,13 @@
 import { DbDeleteRegisterById } from "../../data/usescases/delete-register/db-delete-register";
 import { LogMongoRepository } from "../../infra/db/mongodb/log-repository/log-mongo-repository";
-import { RegisterMongoRepository } from "../../infra/db/mongodb/register-repository/register";
+import { RegisterMySqlRepository } from "../../infra/db/mysql/register-repository/register-mysql-repository";
 import { DeleteRegisterController } from "../../presentation/controllers/delete-register/delete-register";
 import { Controller } from "../../presentation/protocols/controller";
 import { LogControllerDecorator } from "../decorators/log";
 
 export const makeDeleteRegisterByIdController = (): Controller => {
-  const DeleteRegisterByIdRepository = new RegisterMongoRepository();
+  const DeleteRegisterByIdRepository = new RegisterMySqlRepository();
   const deleteRegister = new DbDeleteRegisterById(DeleteRegisterByIdRepository);
   const controllerDeleteById = new DeleteRegisterController(deleteRegister);
-  const logError = new LogMongoRepository();
-  return new LogControllerDecorator(controllerDeleteById, logError);
+  return controllerDeleteById;
 };
