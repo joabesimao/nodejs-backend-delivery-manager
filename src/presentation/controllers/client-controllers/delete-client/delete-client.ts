454567d8fc26760dcc3ae7d9ab2c92fb301b2ad5
joabesimao <simaojoabecosta@gmail.com>
2025-05-13 14:13:46 -0300

    feat: ensure delete one client controller

---
+++ src/presentation/controllers/client-controllers/delete-client/delete-client.ts
@@ -0,0 +1,18 @@
+import { DeleteClient } from "../../../../domain/usescases/client/delete-client";
+import { ok, serverError } from "../../../helpers/http/http-helper";
+import { Controller } from "../../../protocols/controller";
+import { HttpRequest, HttpResponse } from "../../../protocols/http";
+
+export class DeleteClientController implements Controller {
+  constructor(private readonly deleteClient: DeleteClient) {}
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const deletedClient = await this.deleteClient.delete(
+        httpRequest.params.id
+      );
+      return ok(deletedClient)
+    } catch (error) {
+      return serverError(error);
+    }
+  }
+}
