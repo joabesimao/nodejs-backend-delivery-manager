c063de24baf25bb56473af20b0382edbe820ec9d
joabesimao <simaojoabecosta@gmail.com>
2025-05-30 21:14:29 -0300

    feat: ensure create factory db auth-mongoDb

--- src/main/factories/auth-middleware-factory.ts
+++ src/main/factories/auth-middleware-factory.ts
@@ -3,7 +3,7 @@ import { JwtAdapter } from "../../infra/cryptography/jwt-adapter/jwt-adapter";
 import { AccountMongoRepository } from "../../infra/db/mongodb/account-repository/account-repository";
 import { AuthMiddleware } from "../../presentation/middlewares/auth-middleware";
 import { Middleware } from "../../presentation/protocols/middleware";
-import env from "../../env";
+import env from "../../../env";
 
 export const makeAuthMiddleware = (role?: string): Middleware => {
   const jwtAdapter = new JwtAdapter(env.jwtSecret);
--- src/main/factories/db-authentication-factory.ts
+++ src/main/factories/db-authentication-factory.ts
@@ -1,16 +1,15 @@
-import env from "../../env";
+import env from "../../../env";
 import { DbAuthentication } from "../../data/usescases/authentication/db-authentication";
 import { Authentication } from "../../domain/usescases/authentication/authentication";
 import { BcryptAdapter } from "../../infra/cryptography/bcrypt-adapter/bcrypt-adapter";
 import { JwtAdapter } from "../../infra/cryptography/jwt-adapter/jwt-adapter";
-import { AccountMySqlRepository } from "../../infra/db/mysql/account-repository/account-repository";
-import { prisma } from "../../infra/db/mysql/helpers/index";
+import { AccountMongoRepository } from "../../infra/db/mongodb/account-repository/account-repository";
 
 export const makeDbAuthentication = (): Authentication => {
   const salt = 12;
   const bcryptAdapter = new BcryptAdapter(salt);
   const jwt = new JwtAdapter(env.jwtSecret);
-  const accountMysqlRepository = new AccountMySqlRepository(prisma);
+  const accountMysqlRepository = new AccountMongoRepository();
   return new DbAuthentication(
     accountMysqlRepository,
     bcryptAdapter,
