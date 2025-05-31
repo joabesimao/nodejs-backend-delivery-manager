91fc5c618da74c1d03e9eb78581baa2f9571d720
joabesimao <simaojoabecosta@gmail.com>
2025-05-30 21:12:45 -0300

    feat: ensure create factory add-order-delivery

--- src/main/factories/add-order-delivery.ts
+++ src/main/factories/add-order-delivery.ts
@@ -1,12 +1,11 @@
 import { Controller } from "../../presentation/protocols/controller";
 import { DbAddOrderDelivery } from "../../data/usescases/order-delivery/add-order-delivery/db-add-order-delivery";
 import { AddOrderDeliveryController } from "../../presentation/controllers/order-delivery-controllers/add-order-delivery/add-order-delivery";
-import { OrderDeliveryMySqlRepository } from "../../infra/db/mysql/order-delivery-repository/order-delivery-mysql-repository";
-import { prisma } from "../../infra/db/mysql/helpers/index";
 import { makeAddOrderDeliveryValidation } from "./add-order-delivery-validation";
+import { OrderDeliveryMongoRepository } from "../../infra/db/mongodb/order-delivery-repository/order-delivery-repository";
 
-export const makeAddOrderDeliveryController =  (): Controller => {
-  const orderDeliveryRepository = new OrderDeliveryMySqlRepository(prisma);
+export const makeAddOrderDeliveryController = (): Controller => {
+  const orderDeliveryRepository = new OrderDeliveryMongoRepository();
   const addOrderDelivery = new DbAddOrderDelivery(orderDeliveryRepository);
   const orderDelivery = makeAddOrderDeliveryValidation();
   const addOrderDeliveryController = new AddOrderDeliveryController(
