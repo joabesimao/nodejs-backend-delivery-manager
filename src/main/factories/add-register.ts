e5ba6f8ae9dc30a0e4ff469f645f81bc873b49d1
joabesimao <simaojoabecosta@gmail.com>
2025-05-30 21:13:28 -0300

    feat: ensure create factory add-register-mongoDb

--- src/main/factories/add-register.ts
+++ src/main/factories/add-register.ts
@@ -1,14 +1,13 @@
 import { AddRegisterController } from "../../presentation/controllers/register-controllers/add-register/add-register";
 import { DbAddRegister } from "../../data/usescases/register-usecases/add-register/db-add-register";
 import { Controller } from "../../presentation/protocols/controller";
-import { RegisterMySqlRepository } from "../../infra/db/mysql/register-repository/register-mysql-repository";
-import { prisma } from "../../infra/db/mysql/helpers/index";
 import { makeAddRegisterValidation } from "./add-register-validation";
 import { makeAddClientValidation } from "./add-client-validation";
 import { makeAddAddressValidation } from "./add-address-validation";
+import { RegisterMongoRepository } from "../../infra/db/mongodb/register-repository/register";
 
 export const makeAddRegisterController = (): Controller => {
-  const registerRepository = new RegisterMySqlRepository(prisma);
+  const registerRepository = new RegisterMongoRepository();
   const addRegister = new DbAddRegister(registerRepository);
   const validation = makeAddRegisterValidation();
   const validationClient = makeAddClientValidation();
